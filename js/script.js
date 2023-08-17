9351c75e40c198b1000070b97cecd7c8fa74f8c5
joxasabr05 <jaxongirmirhalikov@gmail.com>
2023-08-17 15:17:47 +0500

    Samar 5-dars JS

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//#9. Interpolatsiya
+
+// const channel = prompt("What's your favourite youtuber?");
+//Old
+// console.log("https://youtube.com/" + channel + "/videos");
+// Interpolatsiya
+// console.log(`https://youtube.com/${channel}`);
+
+const user = "Jaxongir";
+const job = "JavaScript developer";
+const age = 17;
+console.log(`User name is ${user} his is ${job} your age is ${age}  `);
